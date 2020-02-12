8751b683f612e82d78e52b2949e3c44a8308a5cd
naka <naka@example.com>
2020-02-13 01:46:23 +0900

    milestoneに属するissue

--- a.php
+++ a.php
@@ -10,4 +10,13 @@ $backlog = new Backlog(new ApiKeyConnector('xxxxx', 'xxxxxxxx','jp'));
 $issues = $backlog->issues->load();
 print_r($issues);
 
+// milestone に所属するissue
+$milestones = $backlog->projects->versions("TP1");
+print_r($milestones[0]);
+$issues = $backlog->issues->load([
+    "milestoneId"=>[
+        $milestones[0]->id,
+    ],
+]);
+print_r($issues);
 
