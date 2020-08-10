abe58810c3bf93d0eeb976d97509723e9307e3bd
naka <naka@example.com>
2020-08-10 09:00:34 +0900

    debug writer

--- a.php
+++ a.php
@@ -5,18 +5,23 @@ require_once "./vendor/autoload.php";
 use Itigoppo\BacklogApi\Backlog\Backlog;
 use Itigoppo\BacklogApi\Connector\ApiKeyConnector;
 
+function echo_json($it){
+    echo json_encode($it, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE|JSON_UNESCAPED_SLASHES);
+}
+
 $backlog = new Backlog(new ApiKeyConnector(getenv("SPACE_ID"), getenv("APIKEY"),'jp'));
 
 $issues = $backlog->issues->load();
-print_r($issues);
+echo_json($issues);
 
 // milestone に所属するissue
 $milestones = $backlog->projects->versions("TP1");
-print_r($milestones[0]);
+echo echo_json($milestones);
 $issues = $backlog->issues->load([
     "milestoneId"=>[
         $milestones[0]->id,
     ],
 ]);
-print_r($issues);
+echo echo_json($issues);
+
 
